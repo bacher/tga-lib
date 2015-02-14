fc3a0af3a3ed0352c668af6420cefddc85700b55
Bacher <bacherko@gmail.com>
2015-02-15 01:52:46 +0300

    rle step 2

--- tga.cpp
+++ tga.cpp
@@ -7,26 +7,26 @@ using namespace std;
 
 namespace TGA {
 
-    void compress(u_int8_t* data, u_int16_t size) {
+    void compress(u_int8_t* data, u_int16_t data_length) {
 
 //        u_int8_t dt[8]{2, 2, 3, 2,  2, 2, 4, 2};
 //
 //        data = dt;
 //        size = 8;
 
+        int size = data_length;
+
         int cnt = 1;
         int diff = 1;
-        u_int8_t prev = data[0];
 
-        for (int i = 1; i <= size; ++i) {
+        for (int i = 3; i <= size; i += 3) {
 
-            u_int8_t current = data[i];
-
-            if (i != size && current == prev) {
+            if (i != size && data[i] == data[i - 3] && data[i + 1] == data[i - 2] && data[i + 2] == data[i - 1]) {
                 cnt++;
 
                 if (diff > 1) {
-                    for (int j = i - diff; j < i - 1; ++j) {
+                    cout << '(' << (diff - 1) << ") ";
+                    for (int j = i - (diff * 3); j < i - 3; j += 3) {
                         cout << (int)data[j] << ' ';
                     }
 
@@ -34,14 +34,16 @@ namespace TGA {
                 }
             } else {
                 if (cnt > 1) {
-                    cout << cnt << '*' << (int)prev << ' ';
+                    cout << cnt << '*' << (int)data[i - 3] << ' ';
                     cnt = 1;
                     diff = 1;
 
                 } else {
 
                     if (i == size) {
-                        for (int j = i - diff; j < i; ++j) {
+
+                        cout << '(' << diff << ") ";
+                        for (int j = i - (diff * 3); j < i; j += 3) {
                             cout << (int)data[j] << ' ';
                         }
                     } else {
@@ -49,8 +51,6 @@ namespace TGA {
                     }
                 }
             }
-
-            prev = current;
 
         }
     };
